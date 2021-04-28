5eee372f78f2d0a0241605963c1bf7aa6593280f
2214631535@qq.com <2214631535@qq.com>
2021-04-28 09:11:04 +0800

    master

---
+++ src/components/contentdetail.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <ul>
+      <router-link to="/based/contentdetail">
+        <li
+          class="item"
+          v-for="(item, index) in passagelist"
+          :key="index"
+          @click="getpassage(item.id)"
+        >
+          <a> {{ item.title }}</a>
+        </li>
+      </router-link>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "contentdetail",
+  data() {
+    return {};
+  },
+  methods: {
+    getpassagecontent() {
+      this.getpassage();
+      this.$axios({
+        url: "http://121.4.187.232:8080/passage/passageResources",
+        method: "get",
+        params: {
+          passageID: this.list.id,
+        },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }).then((res) => {
+        for (var i in res.data) {
+          this.contentlist.push(res.data[i]);
+          console.log(this.contentlist);
+        }
+      });
+    },
+  },
+};
+</script>
+<style scoped>
+</style>
